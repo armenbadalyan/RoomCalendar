cf18201e654ad015563804a1d1b58fb24bee1de9
Armen Badalyan <abadalyan@exadel.com>
2017-02-16 19:12:45 +0300

    clearing timer on component destroy

--- src/app/core/clock/clock.component.ts
+++ src/app/core/clock/clock.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit, NgZone, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, OnDestroy, NgZone, ChangeDetectorRef } from '@angular/core';
 
 @Component({
 	selector: 'clock',
 	templateUrl: './clock.component.html',
 	styleUrls: ['./clock.component.scss']	
 })
-export class ClockComponent implements OnInit {
+export class ClockComponent implements OnInit, OnDestroy {
 
 	time:Date = new Date();
+
+	private timer:number;
 
 	constructor(private zone:NgZone, private  cd: ChangeDetectorRef) {
 		this.runFnBind = this.run.bind(this);
@@ -17,10 +19,14 @@ export class ClockComponent implements OnInit {
 		this.zone.runOutsideAngular(this.runFnBind);
 	}
 
-	run() {
+	ngOnDestroy() {
+		clearTimeout(this.timer);
+	}
+
+	run() {		
 		this.time = new Date();
 		this.cd.detectChanges();
-		setTimeout( this.runFnBind, 1000)
+		this.timer = setTimeout( this.runFnBind, 1000);
 	}
 
 	private runFnBind: any = null;

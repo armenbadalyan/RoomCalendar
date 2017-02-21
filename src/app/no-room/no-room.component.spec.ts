87d9013849869dd2764c09b43022484df1c229df
fr-ench <gryshkevich.maria@gmail.com>
2017-02-21 14:41:53 +0500

    add no room component test

--- src/app/no-room/no-room.component.spec.ts
+++ src/app/no-room/no-room.component.spec.ts
@@ -1,28 +1,70 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, fakeAsync, inject, tick, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Location } from '@angular/common';
+
 import { NoRoomComponent } from './no-room.component';
+import { PageService } from '../shared';
+
+let PageServiceStub;
+
+@Component({
+  template: `<app-no-room></app-no-room><router-outlet></router-outlet>`
+})
+class TestHostComponent {}
+
+@Component({
+  template: ''
+})
+class DummyComponent { }
 
 describe('NoRoomComponent', () => {
-  let component: NoRoomComponent;
+  let hostComponent: TestHostComponent;
   let fixture: ComponentFixture<NoRoomComponent>;
+  let location: Location;
+  let router: Router;
 
   beforeEach(async(() => {
+
+    PageServiceStub = {
+      notifyPageInit: (page) => true
+    };
+
     TestBed.configureTestingModule({
-      declarations: [ NoRoomComponent ]
+      imports: [
+        RouterTestingModule.withRoutes([
+          { path: 'settings', component: DummyComponent }
+        ])
+      ],
+      declarations: [ TestHostComponent, NoRoomComponent, DummyComponent ],
+      providers:    [
+        { provide: PageService, useValue: PageServiceStub }
+      ]
     })
     .compileComponents();
   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(NoRoomComponent);
-    component = fixture.componentInstance;
+  beforeEach(inject([Router, Location], (_router: Router, _location: Location) => {
+    location = _location;
+    router = _router;
+    fixture = TestBed.createComponent(TestHostComponent);
+    hostComponent = fixture.componentInstance;
     fixture.detectChanges();
-  });
+  }));
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(hostComponent).toBeTruthy();
   });
+
+  it('link should go to settings', fakeAsync(() => {
+      fixture.debugElement.query(By.css('a')).nativeElement.click();
+      tick();
+      expect(location.path()).toEqual('/settings');
+  }));
+
 });

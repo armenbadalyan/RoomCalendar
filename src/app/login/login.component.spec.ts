978a8889d84aa089175c079409b7986525094011
fr-ench <gryshkevich.maria@gmail.com>
2017-02-22 21:18:46 +0500

    Basic test for login component creation

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -2,16 +2,51 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
+import { Http, BaseRequestOptions } from '@angular/http';
+import { MockBackend } from '@angular/http/testing';
+
+import { Observable } from 'rxjs/Rx';
 import { LoginComponent } from './login.component';
+import { UserService, PageService } from '../shared';
+
+let UserServiceStub,
+    PageServiceStub,
+    RouterStub,
+    userService;
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
   beforeEach(async(() => {
+    UserServiceStub = {
+      login: function(username, password) {
+        return Observable.create((observer) => {
+          observer.next(true);
+          observer.complete();
+        });
+      }
+    };
+
+    PageServiceStub = {
+      notifyPageInit: (page) => true
+    };
+
+    RouterStub = {
+      navigate: urlArray => urlArray.length ? urlArray[0] : null
+    };
+
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      imports: [ FormsModule ],
+      declarations: [ LoginComponent ],
+      providers:    [
+        { provide: UserService, useValue: UserServiceStub },
+        { provide: PageService, useValue: PageServiceStub },
+        { provide: Router,      useValue: RouterStub }
+      ]
     })
     .compileComponents();
   }));
@@ -19,6 +54,8 @@ describe('LoginComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
+    userService = TestBed.get(UserService);
+
     fixture.detectChanges();
   });
 

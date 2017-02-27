de6026d4c0396c919830040f66ac76be38363f01
fr-ench <gryshkevich.maria@gmail.com>
2017-02-27 14:12:01 +0500

    add header component test

--- src/app/core/layout/header/header.component.spec.ts
+++ src/app/core/layout/header/header.component.spec.ts
@@ -1,27 +1,47 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, fakeAsync, tick, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement, Component } from '@angular/core';
 
+import { Observable } from 'rxjs/Rx';
 import { Router } from '@angular/router';
-import { RouterTestingModule } from '@angular/router/testing';
 import { Location } from '@angular/common';
+import { PageService, Page } from '../../../shared';
+import { Subject }    from 'rxjs/Subject';
 
 import { HeaderComponent } from './header.component';
 
 @Component({
   selector: 'clock',
-  template: ``
+  template: ''
 })
 class ClockComponent { }
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let pageInitializedSource: Subject<any>;
+  let pageService;
+  let PageServiceStub;
+  let RouterStub;
 
   beforeEach(async(() => {
+    pageInitializedSource = new Subject();
+
+    PageServiceStub = {
+      pageInitialized: pageInitializedSource.asObservable()
+    };
+
+    RouterStub = {
+      navigate: (urlArray) => urlArray.length ? urlArray[0] : null
+    };
+
     TestBed.configureTestingModule({
-      declarations: [ HeaderComponent, ClockComponent ]
+      declarations: [ HeaderComponent, ClockComponent ],
+      providers:    [
+        { provide: PageService, useValue: PageServiceStub },
+        { provide: Router,      useValue: RouterStub }
+      ]
     })
     .compileComponents();
   }));
@@ -32,7 +52,43 @@ describe('HeaderComponent', () => {
     fixture.detectChanges();
   });
 
-  /*it('should create', () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
-  });*/
+  });
+
+  it('should set positive properties from PageService', fakeAsync(() => {
+    pageInitializedSource.next({
+      title: 'Test header',
+      hasMenu: true,
+      isTitleCentered: true
+    });
+
+    tick();
+    fixture.detectChanges();
+
+    let title = fixture.debugElement.query(By.css('.app-header__title'));
+    let menu = fixture.debugElement.query(By.css('.app-header__button--menu'));
+    let titleEl = title.nativeElement;
+    expect(titleEl.textContent).toEqual('Test header');
+    expect(titleEl.className.indexOf('app-header__title--centered')).toBeGreaterThanOrEqual(0);
+    expect(menu).toBeTruthy();
+  }));
+
+  it('should set negative properties from PageService', fakeAsync(() => {
+    pageInitializedSource.next({
+      title: 'Test header',
+      hasMenu: false,
+      isTitleCentered: false
+    });
+
+    tick();
+    fixture.detectChanges();
+
+    let title = fixture.debugElement.query(By.css('.app-header__title'));
+    let menu = fixture.debugElement.query(By.css('.app-header__button--menu'));
+    let titleEl = title.nativeElement;
+    expect(titleEl.textContent).toEqual('Test header');
+    expect(titleEl.className.indexOf('app-header__title--centered')).toBe(-1);
+    expect(menu).toBeFalsy();
+  }));
 });

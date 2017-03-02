ea48ae4c8bc526139f777f2b340b3246c73b6bba
fr-ench <gryshkevich.maria@gmail.com>
2017-03-02 14:11:57 +0500

    add debugging info

--- server/authorize-google.js
+++ server/authorize-google.js
@@ -5,14 +5,19 @@ var Promise = require('promise');
 var authPromise = new Promise(function(resolve, reject){
 
  var fileName = process.env.GOOGLE_APPLICATION_CREDENTIALS || null,
-     file = fileName ? require(path.resolve(fileName)) : null;
+     file = fileName ? require(path.resolve(fileName)) : null,
+     client_email = file ? file.client_email : process.env.client_email,
+     private_key = file ? file.private_key : process.env.private_key,
+     impresonate_email = file ? file.impersonate_email : process.env.impersonate_email;
+
+  console.log('file is ' +  !!file);
 
   var jwtClient = new google.auth.JWT(
-    file.client_email || process.env.client_email || "",
+    client_email || "",
     null,
-    (file.private_key || process.env.private_key || "").replace(/\\n/g, '\n'),
+    (private_key || "").replace(/\\n/g, '\n'),
     [ 'https://www.googleapis.com/auth/calendar.readonly' ],
-    file.impersonate_email || process.env.impersonate_email  || null
+    impresonate_email  || null
   );
 
   jwtClient.authorize( function( err, tokens ) {

88dc569475e2e7fc36224e5fdd79ec4f4dad4b6c
Armen Badalyan <abadalyan@exadel.com>
2017-03-07 12:30:56 +0300

    fixed arcade module in tests

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -2,7 +2,7 @@
 import { async, fakeAsync, tick, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { Component, Input, DebugElement } from '@angular/core';
-import { QRCodeModule } from 'angular2-qrcode';
+import { QRCodeModule } from 'ng2-qrcode';
 import { Subject }    from 'rxjs/Subject';
 import { ActivatedRoute } from '@angular/router';
 import { HomeComponent } from './home.component';

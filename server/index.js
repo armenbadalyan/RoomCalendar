73976c327f355b85a763f48981b7760a14ab2bc5
fr-ench <gryshkevich.maria@gmail.com>
2017-03-09 18:06:51 +0500

    add server side url to insert events

--- server/index.js
+++ server/index.js
@@ -31,19 +31,78 @@ var calendar = google.calendar('v3');
 
 app.get('/api/calendars', function (req, res) {
   calendar.calendarList.list({}, function (err, result) {
+    if(!result || typeof result.items == 'undefined') {
+      res.sendStatus(400);
+      return;
+    }
     res.json(result.items || []);
   });
 });
 
 app.get('/api/events', function (req, res) {
+  var query = req.query;
+
+  if(typeof query.calendarId == 'undefined' || typeof query.time == "undefined" || typeof query.limit == 'undefined') {
+    console.log('undefined');
+    res.sendStatus(400);
+    return;
+  }
+
   calendar.events.list({
       showDeleted: false,
       singleEvents: true,
       orderBy: 'startTime',
-      calendarId: req.query.calendarId,
-      timeMin: req.query.time,
-      maxResults: req.query.limit || 10,
+      calendarId: query.calendarId,
+      timeMin: query.time,
+      maxResults: query.limit || 10,
   }, function (err, result) {
+      if(!result || typeof result.items == 'undefined') {
+        res.sendStatus(400);
+        return;
+      }
+
       res.json(result.items || []);
   });
+});
+
+app.get('/api/insert', function (req, res) {
+  var query = req.query;
+
+  if(typeof query.creator == 'undefined' || typeof query.calendarId == 'undefined') {
+    res.sendStatus(400);
+    return;
+  }
+
+  AuthorizeGoogle(query.creator).then(function() {
+    calendar.events.insert({
+        'calendarId': query.calendarId,
+        'resource': {
+          'summary': query.title || 'No Name',
+          'description': query.description || '',
+          'start': {
+            'dateTime':  query.startTime
+          },
+          'end': {
+            'dateTime':  query.endTime
+          },
+          'reminders': {
+            'useDefault': true
+          }
+        }
+      }, function(err, result) {
+        if(err) {
+          res.json({
+            success: false,
+            error: err
+          });
+        } else {
+            res.json({
+              success: true,
+              event: result
+            });
+        }
+    });
+  });
+
+  
 });

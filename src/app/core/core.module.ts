bfefede74253ca0fdcec3a7bd5407d40c4c7e99b
fr-ench <gryshkevich.maria@gmail.com>
2017-03-16 16:57:57 +0500

    Replace icon implementation via images with SVG sprites

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { HeaderComponent } from './layout/header';
 import { ClockComponent } from './clock/clock.component';
+import { InlineSVGModule } from 'ng-inline-svg';
 
 @NgModule({
 	imports: [
 		CommonModule,
-		SharedModule
+		SharedModule,
+		InlineSVGModule
 	],
 	declarations: [HeaderComponent, ClockComponent],
-	exports: [HeaderComponent]
+	exports: [HeaderComponent, InlineSVGModule]
 })
 export class CoreModule { }

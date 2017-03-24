5444e112720cc2149b580fbff4a12d0ffd970eb3
fr-ench <gryshkevich.maria@gmail.com>
2017-03-24 10:45:20 +0500

    Add more profound error handling and logging

--- server/authorize-google.js
+++ server/authorize-google.js
@@ -9,12 +9,13 @@ var authPromise = function () {
             file = fileName ? require(path.resolve(fileName)) : null,
             client_email = file ? file.client_email : process.env.client_email,
             private_key = file ? file.private_key : process.env.private_key,
-            impresonate_email = file ? file.impersonate_email : process.env.impersonate_email;
+            impresonate_email = file ? file.impersonate_email : process.env.impersonate_email,
+            scope = file ? file.scope : process.env.scope;
 
         var jwtClient = new google.auth.JWT(
             client_email || "",
             null,
-            (private_key || "").replace(/\\n/g, '\n'), ['https://www.googleapis.com/auth/calendar.readonly'],
+            (private_key || "").replace(/\\n/g, '\n'), [scope],
             impresonate_email  || null
         );
 
--- server/index.js
+++ server/index.js
@@ -31,19 +31,40 @@ var calendar = google.calendar('v3');
 
 app.get('/api/calendars', function (req, res) {
   calendar.calendarList.list({}, function (err, result) {
+    if(!result || typeof result.items == 'undefined') {
+      console.log('err during request');
+      console.log(err);
+      res.sendStatus(400);
+      return;
+    }
     res.json(result.items || []);
   });
 });
 
 app.get('/api/events', function (req, res) {
+  var query = req.query;
+
+  if(typeof query.calendarId == 'undefined' || typeof query.time == "undefined" || typeof query.limit == 'undefined') {
+    console.log('undefined parametrs');
+    res.sendStatus(400);
+    return;
+  }
+
   calendar.events.list({
       showDeleted: false,
       singleEvents: true,
       orderBy: 'startTime',
-      calendarId: req.query.calendarId,
-      timeMin: req.query.time,
-      maxResults: req.query.limit || 10,
+      calendarId: query.calendarId,
+      timeMin: query.time,
+      maxResults: query.limit || 10,
   }, function (err, result) {
+      if(!result || typeof result.items == 'undefined') {
+        console.log('err during request');
+        console.log(err);
+        res.sendStatus(400);
+        return;
+      }
+
       res.json(result.items || []);
   });
 });
